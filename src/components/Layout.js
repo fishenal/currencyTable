3618cd65c4761f35374b31c8c2b933e63e55a658
Yudonghan <Donghan.Yu@ge.com>
2020-09-23 11:52:40 +0800

    materialui and react router to make a pure and simple version

---
+++ src/components/Layout.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import {
+  Route,
+  Switch,
+  useLocation,
+  useHistory,
+} from "react-router-dom";
+import Currencies from '../pages/Currencies';
+import Setting from '../pages/Setting';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: '100vh',
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export default function Layout() {
+  const classes = useStyles();
+  const location = useLocation();
+  const history = useHistory();
+  const path = location.pathname;
+
+  const handleChange = (event, newValue) => {
+    history.push(newValue)
+  };
+
+  return (
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={path}
+        onChange={handleChange}
+        className={classes.tabs}
+      >
+        <Tab label="Currencies" value="/currencies"/>
+        <Tab label="Setting" value="/setting"/>
+      </Tabs>
+      <Switch>
+        <Route path="/currencies">
+          <Box p={3}>
+            <Typography><Currencies /></Typography>
+          </Box>
+        </Route>
+        <Route path="/setting">
+          <Box p={3}>
+            <Typography><Setting /></Typography>
+          </Box>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
---
+++ src/pages/Currencies.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Currencies() {
+  return (
+    <div>
+      currencies
+    </div>
+  )
+}
